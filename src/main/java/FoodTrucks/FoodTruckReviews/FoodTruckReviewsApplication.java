05dd475f8be2af4696914e219638352a8aafe539
kelleyryanj <kelley.ryanj@gmail.com>
2019-06-29 15:56:29 -0400

    first commit

---
+++ src/main/java/FoodTrucks/FoodTruckReviews/FoodTruckReviewsApplication.java
@@ -0,0 +1,13 @@
+package FoodTrucks.FoodTruckReviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckReviewsApplication.class, args);
+	}
+
+}
