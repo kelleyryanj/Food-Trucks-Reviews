a80ad96e1513fe83cf5375db7a1870b59cfb1a8c
kelleyryanj <kelley.ryanj@gmail.com>
2019-07-03 19:50:07 -0400

    rjk

--- src/main/java/foodTruckReviews/FoodtruckRepository.java
+++ src/main/java/foodTruckReviews/FoodtruckRepository.java
@@ -4,9 +4,7 @@ import java.util.Collection;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface FoodtruckRepository extends CrudRepository<Foodtruck, Long> {
-
 
 	Collection<Foodtruck> findByCuisinesContains(Cuisine cuisine);
 
